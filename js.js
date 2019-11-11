03892acc5cd1cdea52b58d7fcaba8ab5fe0a6537
richard239961 <56685942+richard239961@users.noreply.github.com>
2019-11-11 10:27:13 +0000

    [rd] Cleaning Questions up. Added title picker
    + Cleaned |Drop Down boxers

--- js.js
+++ js.js
@@ -23,3 +23,28 @@ function filterFunction() {
 function random() {
   document.getElementById("gen").innerHTML = Math.random();
 }
+
+function title() {
+  a = document.getElementById("title").value
+  if(a=="Question one"){
+    document.getElementById("document.getElementById('id01').style.display='block'")
+  } else if(a=="Question one"){
+      document.getElementById("document.getElementById('id02').style.display='block'")
+  } else if(a=="Question Three"){
+      document.getElementById("document.getElementById('id03').style.display='block'")
+  } else if(a=="Question Four"){
+      document.getElementById("document.getElementById('id04').style.display='block'")
+  } else if(a=="Question Five"){
+      document.getElementById("document.getElementById('id05').style.display='block'")
+  } else if(a=="Question Six"){
+      document.getElementById("document.getElementById('id06').style.display='block'")
+  } else if(a=="Question Seven"){
+      document.getElementById("document.getElementById('id07').style.display='block'")
+  } else if(a=="Question Eight"){
+      document.getElementById("document.getElementById('id08').style.display='block'")
+  } else if(a=="Question Nine"){
+      document.getElementById("document.getElementById('id09').style.display='block'")
+  } else (a=="Question Ten"){
+     document.getElementById("document.getElementById('id10').style.display='block'")
+   }
+}
