6cb8ea78f8c469a3c7d668ff13a6143e61e0aa20
richard239961 <56685942+richard239961@users.noreply.github.com>
2019-11-12 23:19:56 +0000

    [rd] CSS, JS. INDEX, MAIN PAGE, CALIMS(All) read long
    DONE:
    Re-write most of css.
    Done Js.
    Done index
        - Lost Register buttons
        - Added "forgot password"
        - Lost forgot password buttons
        - Added Logo
        - Fixed modals
    Done Main Page
        - Fixed Account modal
        - Fixed Contact modal
        - Removed Support modal
        - Fixed Logout Modal
        - Fixed Drop down
        - Logo Broken.
        - Changed postion of title & drop down
        - Fixed Policies & buttons
        - Tried to add search [FAILED]
    CLAIMS
      Fire - Done
      Home - Done
      Motor - Done
      Pet - Done
      Questions
        - Changed Questions
        -Ficed Logo
        - Fixed All modal
      Technology - Done
      Theft - Done
      unable_fire
      unable_home
      unable_pet
      unable_theft
      unable_tech

--- js.js
+++ js.js
@@ -1,16 +1,12 @@
-var pon = '234112'
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-function filterFunction() {
+function myFunction() /* When the user clicks on the button, toggle between hiding and showing the dropdown content */ {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+function filterFunction() /*Filter funtion for dropdown not being used*/ {
   var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
+  div = document.getElementById("polices");
+  a = div.getElementsByTagName("a, h1");
   for (i = 0; i < a.length; i++) {
     txtValue = a[i].textContent || a[i].innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -19,32 +15,4 @@ function filterFunction() {
       a[i].style.display = "none";
     }
   }
-}
-function random() {
-  document.getElementById("gen").innerHTML = Math.random();
-}
-
-function title() {
-  a = document.getElementById("title").value
-  if(a=="Question one"){
-    document.getElementById("document.getElementById('id01').style.display='block'")
-  } else if(a=="Question one"){
-      document.getElementById("document.getElementById('id02').style.display='block'")
-  } else if(a=="Question Three"){
-      document.getElementById("document.getElementById('id03').style.display='block'")
-  } else if(a=="Question Four"){
-      document.getElementById("document.getElementById('id04').style.display='block'")
-  } else if(a=="Question Five"){
-      document.getElementById("document.getElementById('id05').style.display='block'")
-  } else if(a=="Question Six"){
-      document.getElementById("document.getElementById('id06').style.display='block'")
-  } else if(a=="Question Seven"){
-      document.getElementById("document.getElementById('id07').style.display='block'")
-  } else if(a=="Question Eight"){
-      document.getElementById("document.getElementById('id08').style.display='block'")
-  } else if(a=="Question Nine"){
-      document.getElementById("document.getElementById('id09').style.display='block'")
-  } else (a=="Question Ten"){
-     document.getElementById("document.getElementById('id10').style.display='block'")
-   }
 }
